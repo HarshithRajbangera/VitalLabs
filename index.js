2d003500d7433af6062be139db0832c3ddc86e2a
Harshith <harshithbangera13@gmail.com>
2025-08-01 16:24:38 +0530

    New change

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+});
